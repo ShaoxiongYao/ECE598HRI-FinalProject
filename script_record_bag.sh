ac5228799a47dbef96f827dfe1ab466f5661841e
GPU-Computer <GPU-Computer>
2022-04-20 12:02:05 -0500

    update scripts

--- script_record_bag.sh
+++ script_record_bag.sh
@@ -2,7 +2,9 @@
 set -Eeuo pipefail
 set -x
 
-rosbag record /cam_left/color/image_raw /cam_right/color/image_raw /cam_torso/color/image_raw\
+rosbag record /cam_left/color/image_raw\
+              /cam_right/color/image_raw\
+            #   /cam_torso/color/image_raw\
               /cam_left/aligned_depth_to_color/image_raw\
-              /cam_right/aligned_depth_to_color/image_raw\
-              /cam_torso/aligned_depth_to_color/image_raw
+              /cam_right/aligned_depth_to_color/image_raw
+            #   /cam_torso/aligned_depth_to_color/image_raw
--- test_sh.py
+++
@@ -1,5 +0,0 @@
-import time
-
-for i in range(100):
-    print("hello:", i)
-    time.sleep(1)
