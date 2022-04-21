1ebe28405ec0c5c2969418c656ddfcef47c43335
GPU-Computer <GPU-Computer>
2022-04-21 15:46:29 -0500

    move topic name in iteration

--- bag2png.py
+++ bag2png.py
@@ -8,52 +8,53 @@ import numpy as np
 from pathlib import Path
 
 ROOT_DIR = 'Dataset'
-BAG_NAME = '2022-04-19-20-27-48'
+BAG_NAME = '2022-04-20-20-51-50'
 
-cam_name  = 'cam_right'
-cam_topic = 'cam_topic3'
 save_imgs = True
 
 if __name__ == '__main__':
-    FILENAME = os.path.join(ROOT_DIR, 'rosbags', BAG_NAME+'.bag')
-    bag = rosbag.Bag(FILENAME)
-    for i in range(2):
-        if (i == 0):
-            TOPIC = f'/{cam_topic}/aligned_depth_to_color/image_raw'
-            DESCRIPTION = 'depth_'
-        else:
-            TOPIC = f'/{cam_topic}/color/image_raw'
-            DESCRIPTION = 'color_'
-        image_topic = bag.read_messages(TOPIC)
+    for cam_topic in ['cam_left','cam_right','cam_torso']:
+        cam_name = cam_topic
+        FILENAME = os.path.join(ROOT_DIR, 'rosbags', BAG_NAME+'.bag')
+        bag = rosbag.Bag(FILENAME)
+        for i in range(2):
+            if (i == 0):
+                TOPIC = f'/{cam_topic}/aligned_depth_to_color/image_raw'
+                DESCRIPTION = 'depth_'
+            else:
+                TOPIC = f'/{cam_topic}/color/image_raw'
+                DESCRIPTION = 'color_'
+            image_topic = bag.read_messages(TOPIC)
 
-        timestamp_lst = []
-        for k, b in enumerate(image_topic):
+            timestamp_lst = []
+            for k, b in enumerate(image_topic):
 
-            if save_imgs:
-                bridge = CvBridge()
-                cv_image = bridge.imgmsg_to_cv2(b.message, b.message.encoding)
-                cv_image.astype(np.uint8)
+                if save_imgs:
+                    bridge = CvBridge()
+                    cv_image = bridge.imgmsg_to_cv2(b.message, b.message.encoding)
+                    cv_image.astype(np.uint8)
 
-                img_dir =  f'{ROOT_DIR}/{BAG_NAME}/{cam_name}/{DESCRIPTION[:-1]}/'
-                Path(img_dir).mkdir(parents=True, exist_ok=True)
-                img_fn = DESCRIPTION + str(b.timestamp) + '.png'
+                    img_dir =  f'{ROOT_DIR}/{BAG_NAME}/{cam_name}/{DESCRIPTION[:-1]}/'
+                    Path(img_dir).mkdir(parents=True, exist_ok=True)
+                    img_fn = DESCRIPTION + str(b.timestamp) + '.png'
 
-                img_path = os.path.join(img_dir, img_fn)
-                if (DESCRIPTION == 'depth_'):
-                    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(cv_image, alpha=0.03), cv2.COLORMAP_JET)
-                    cv2.imwrite(img_path, cv_image)
-                else:
-                    cv2.imwrite(img_path, cv_image)
-                print("image path:", img_path)
-                print('saved: ' + img_fn)
+                    img_path = os.path.join(img_dir, img_fn)
+                    if (DESCRIPTION == 'depth_'):
+                        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(cv_image, alpha=0.03), cv2.COLORMAP_JET)
+                        cv2.imwrite(img_path, cv_image)
+                    else:
+                        img_rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+                        cv2.imwrite(img_path, img_rgb)
+                    print("image path:", img_path)
+                    print('saved: ' + img_fn)
 
-            timestamp_lst.append(b.timestamp.to_sec())
+                timestamp_lst.append(b.timestamp.to_sec())
 
-        if timestamp_lst != []:
-            print("start time:", min(timestamp_lst))
-            print("end time:", max(timestamp_lst))
-            timestamp_diff = np.diff(timestamp_lst)
-            print("average frequency:", 1/np.mean(timestamp_diff))
+            if timestamp_lst != []:
+                print("start time:", min(timestamp_lst))
+                print("end time:", max(timestamp_lst))
+                timestamp_diff = np.diff(timestamp_lst)
+                print("average frequency:", 1/np.mean(timestamp_diff))
 
     bag.close()
 
