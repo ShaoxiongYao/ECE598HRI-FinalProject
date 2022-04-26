837b4ea2edfd78b273458e4b8e166677a3c3982f
ShaoxiongYao <49648374+ShaoxiongYao@users.noreply.github.com>
2022-04-26 11:14:47 -0500

    init planning code

--- klampt_load_model.py
+++ klampt_load_model.py
@@ -6,6 +6,11 @@ from klampt import vis
 
 from klampt.plan import robotplanning
 
+def get_home_config():
+    q_home = [0]*29
+    q_home[7] += 1
+    return q_home
+
 if __name__ == '__main__':
     # load robot arm
     world = WorldModel()
@@ -17,12 +22,25 @@ if __name__ == '__main__':
     vis.add("world", world)
     vis.show()
 
-    qgoal = robot.getConfig()
-    # qgoal[3] += 2.0   #move the 4th joint 2 radians from the start
-    # planner = robotplanning.planToConfig(world,robot,qgoal)
-    # path = planner.getPath()
-    # print("path type:", type(path))
-    
+    q_home = get_home_config()
+    robot.setConfig(q_home)
+    time.sleep(10)
+
+    q_goal = robot.getConfig()
+    q_goal[8] -= 1.0   #move the 4th joint 2 radians from the start
+    planner = robotplanning.planToConfig(world, robot, q_goal)
+    path = planner.getPath()
+    print("path type:", type(path))
+    input()
+
+    # space = RobotCSpace(robot,collide.WorldCollider(world))
+    # # (Can also create it without the collider to ignore all self-and environment collisions)
+    # #Optionally:
+    # #Call space.addFeasibilityTest(func,name=None) on the space with as many additional feasibility tests as you want
+    # qinit = robot.getConfig()
+    # qgoal = robot.getConfig()
+    # qgoal[7] += 2.0       #move 2 radians on joint 3
+
     print("q goal len:", len(qgoal))
     
     left_arm_joints = list(range(7, 14))
