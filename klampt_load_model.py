15a002de0a055d6f5edb8351e3f48f0d7ca51e4b
Shaoxiong Yao <syao16@illinois.edu>
2022-04-28 00:07:37 -0500

    changed update frequency

--- klampt_load_model.py
+++ klampt_load_model.py
@@ -3,6 +3,7 @@ import time
 import pickle
 import klampt
 import numpy as np
+import pandas as pd
 from klampt import WorldModel, vis
 from klampt.model import ik, geometry
 from klampt.plan import robotplanning
@@ -37,7 +38,6 @@ def load_hand_seq(gt_data_path, key):
         data_df = pickle.load(f)
     return data_df[key]
 
-
 R_I3 = [1, 0, 0, 0, 1, 0, 0, 0, 1]
 
 if __name__ == '__main__':
@@ -63,6 +63,9 @@ if __name__ == '__main__':
     right_EE_link = robot.link('right_shield_link')
 
     robot_com = np.array(robot.getCom())
+    right_shoulder_link = robot.link('right_shoulder_link')
+    reach_center = np.array(right_shoulder_link.getWorldPosition([0, 0, 0]))
+    print('reach center:', reach_center)
     
     local_p1_box = geometry.box(0.05, 0.05, 0.05, center=[0, 0, 0])
     vis.add('local_p1_box', local_p1_box)
@@ -75,33 +78,48 @@ if __name__ == '__main__':
     hand_box = geometry.box(0.05, 0.05, 0.05, center=[0, 0, 0])
     vis.add(f'hand_box', hand_box)
 
-    key = 'gt_left_hand'
-    hand_seq = load_hand_seq('Dataset/first_ground_truth_alice_no_sword1.pkl', key)
+    df = pd.read_pickle('Dataset/first_ground_truth_alice_no_sword1.pkl')
+
+    color_path_seq = df['cam_torso_color']
+    depth_path_seq = df['cam_torso_depth']
+    hand_seq = df['gt_left_hand']
 
     local_p1 = [0, 0, 0]
 
     # distance to clamp point position
     clamp_thres = 0.7
 
-    for step_idx, hand_center in enumerate(hand_seq[::10]):
+    feasible_check = lambda : not robot.selfCollides()
+    prev_hand_center = None
+
+    for step_idx, hand_center in enumerate(hand_seq):
         print("step:", step_idx)
         trans_center = np.array(R).reshape(3, 3).T @ hand_center + np.array(t) 
         hand_box.setCurrentTransform(R_I3, trans_center)
+
+        if prev_hand_center is not None:
+            hand_diff_l2 = np.linalg.norm(trans_center - prev_hand_center)
+
+            # ignore spurious detection
+            if hand_diff_l2 > 0.5:
+                continue
+        prev_hand_center = trans_center
 
         local_p1_inW = np.array(right_EE_link.getWorldPosition(local_p1))
         local_p1_box.setCurrentTransform(R_I3, local_p1_inW)
 
         world_normal = trans_center - robot_com
         world_normal /= np.linalg.norm(world_normal)
-        local_normal = np.array(right_EE_link.getLocalDirection(world_normal))
 
         world_p1 = trans_center
-        if np.linalg.norm(world_p1-robot_com) > clamp_thres:
-            world_p1 = robot_com + clamp_thres*world_normal
+        if np.linalg.norm(world_p1-reach_center) > clamp_thres:
+            reach_normal = world_p1 - reach_center
+            reach_normal /= np.linalg.norm(reach_normal)
+            world_p1 = reach_center + clamp_thres*reach_normal
         world_p1_box.setCurrentTransform(R_I3, world_p1)
 
         lambda1 = 0.1
-        local_p2 = local_p1 + lambda1*local_normal
+        local_p2 = local_p1 + lambda1*np.array([0, 0, 1])
         local_p2_inW = np.array(right_EE_link.getWorldPosition(local_p2))
         local_p2_box.setCurrentTransform(R_I3, local_p2_inW)
         world_p2 = world_p1 + lambda1*world_normal
@@ -115,18 +133,12 @@ if __name__ == '__main__':
         prev_config = robot.getConfig()
         vis.lock()
         solve_result = ik.solve_global(ik_obj, iters=1000, tol=1e-3, activeDofs=right_arm_joints,
-                                       numRestarts=100, feasibilityCheck=None, startRandom=False)
+                                       numRestarts=100, feasibilityCheck=feasible_check, startRandom=False)
+        curr_config = robot.getConfig()
         if not solve_result:
             robot.setConfig(prev_config)
         vis.unlock()
         print("ik result:", solve_result)
 
         vis.update()
-        time.sleep(1)
-
-        # q_goal = robot.getConfig()
-        # q_goal[17] += 1.0
-        # planner = robotplanning.planToConfig(world, robot, q_goal)
-        # print("planner:", planner)
-        # q_path = planner.getPath()
-        # print("q path:", q_path)
+        time.sleep(0.01)
